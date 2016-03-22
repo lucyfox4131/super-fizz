27376d59e4f65afc1bd8df269165a7a1477c32d1
Lucy Fox <lucyreneefox@gmail.com>
2016-03-21 23:10:19 -0600

    cleaned up

--- superfizz.rb
+++ superfizz.rb
@@ -1,11 +1,8 @@
 #supefizz program
+# start with an empty string
+# ADD strings to our starter string if conditional values are true
 
-array = (0..1000)
-
-
-array.each do |number|
-  # start with an empty string
-  # ADD strings to our starter string if conditional values are true
+(0..1000).each do |number|
   to_return = ""
   if number == 0
     puts number # put 0 if on zero
@@ -13,11 +10,9 @@ array.each do |number|
     if number % 7 == 0
       to_return = to_return + "Super"
     end
-
     if number % 5 == 0
       to_return = to_return + "Buzz"
     end
-
     if number % 3 == 0
       to_return = to_return + "Fizz"
     end
