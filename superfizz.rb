cda202c183b653225ed95b0b0955a11d16caf6e1
Lucy Fox <lucyreneefox@gmail.com>
2016-03-21 21:40:27 -0600

    if else superfizz solution

---
+++ superfizz.rb
@@ -0,0 +1,24 @@
+
+array = (0..1000)
+
+array.each do |number|
+  if number == 0
+    puts 0
+  elsif number % 3 == 0 && number % 5 == 0 && number % 7 == 0
+    puts "SuperFizzBuzz"
+  elsif number % 3 == 0 && number % 7 == 0
+    puts "SuperFizz"
+  elsif number % 5 == 0 && number % 7 == 0
+    puts "SuperBuzz"
+  elsif number % 3 == 0 && number % 5 == 0
+    puts "FizzBuzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  elsif number % 5 == 0
+    puts "Buzz"
+  elsif number % 7 == 0
+    puts "Super"
+  else
+    puts number
+  end
+end
