65d5ca9255a7741cf6f0a1adb050e0a9d4519b1d
Lucy Fox <lucyreneefox@gmail.com>
2016-03-21 22:59:11 -0600

    simpler if else superfizz

--- superfizz.rb
+++ superfizz.rb
@@ -2,24 +2,42 @@
 
 array = (0..1000)
 
+
 array.each do |number|
+  # start with an empty string
+  # ADD strings to our starter string if conditional values are true
+  to_return = ""
   if number == 0
-    puts 0
-  elsif number % 3 == 0 && number % 5 == 0 && number % 7 == 0
-    puts "SuperFizzBuzz"
-  elsif number % 3 == 0 && number % 7 == 0
-    puts "SuperFizz"
-  elsif number % 5 == 0 && number % 7 == 0
-    puts "SuperBuzz"
-  elsif number % 3 == 0 && number % 5 == 0
-    puts "FizzBuzz"
-  elsif number % 3 == 0
-    puts "Fizz"
-  elsif number % 5 == 0
-    puts "Buzz"
-  elsif number % 7 == 0
-    puts "Super"
+    puts number # put 0 if on zero
   else
-    puts number
+    if number % 7 == 0
+      to_return = to_return + "Super"
+    end
+
+    if number % 5 == 0
+      to_return = to_return + "Buzz"
+    end
+
+    if number % 3 == 0
+      to_return = to_return + "Fizz"
+    end
+
+    if to_return == ""
+      puts number
+    else
+      puts to_return
+    end
   end
 end
+
+
+
+
+# elsif number % 3 == 0 && number % 5 == 0 && number % 7 == 0
+#   puts "SuperFizzBuzz"
+# elsif number % 3 == 0 && number % 7 == 0
+#   puts "SuperFizz"
+# elsif number % 5 == 0 && number % 7 == 0
+#   puts "SuperBuzz"
+# elsif number % 3 == 0 && number % 5 == 0
+#   puts "FizzBuzz"
